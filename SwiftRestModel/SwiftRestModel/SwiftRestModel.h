10334f843e2177dd4f0fc0b67f470f499299ddf4
Julien Goudet <jgoudet@spir.fr>
2016-02-08 17:37:50 +0100

    Carthage primary support
    * test failed indead

---
+++ SwiftRestModel/SwiftRestModel/SwiftRestModel.h
@@ -0,0 +1,17 @@
+//
+//  SwiftRestModel.h
+//  SwiftRestModel
+//
+//  Created by julien.GOUDET on 08/02/2016.
+//  Copyright © 2016 jurajhilje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRestModel.
+FOUNDATION_EXPORT double SwiftRestModelVersionNumber;
+
+//! Project version string for SwiftRestModel.
+FOUNDATION_EXPORT const unsigned char SwiftRestModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRestModel/PublicHeader.h>
